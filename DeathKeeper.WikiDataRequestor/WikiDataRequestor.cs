673ba1fe16bb2bc86cadecc910d81375cd34143c
Thomas Holmes <thomasholmes@outlook.com>
2015-12-31 02:52:25 +0000

    Added command line args project.

--- DeathKeeper.WikiDataRequestor/WikiDataRequestor.cs
+++ DeathKeeper.WikiDataRequestor/WikiDataRequestor.cs
@@ -5,24 +5,28 @@ using System.Linq;
 using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using Cache;
+using System.IO;
 
 namespace DeathKeeper.WikiData
 {
     public class WikiDataRequestor
     {
+        private WebCache cache;
         private readonly string url = "https://www.wikidata.org/wiki/Special:EntityData/Q{0}.json";
+
+        public WikiDataRequestor()
+        {
+            cache = new WebCache(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "DeathKeeperCache"));
+        }
 
         public WikiDataResponse GetEntity(int entityId)
         {
             var instanceUrl = string.Format(url, entityId);
 
-            using (var webclient = new WebClient())
-            {
-                string body = webclient.DownloadString(instanceUrl);
-                var result = ResultFromString(body);
-                return result;
-            }
-
+            var body = cache.GetPage(instanceUrl);
+            var result = ResultFromString(body);
+            return result;
         }
 
         public WikiDataResponse ResultFromString(string body)
